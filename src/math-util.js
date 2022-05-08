fea31f3c216454ae1d3114e60d569218cd9b10a5
Silas Stoffel <silasstofel@gmail.com>
2022-05-08 19:25:23 -0300

    refactor: removed code smell

--- src/math-util.js
+++ src/math-util.js
@@ -9,23 +9,16 @@ class MathUtil {
 
     sumArray(args) {
         let value = 0;
-        for (let i = 0; i < args.length; i++) {
-            value += args[i];
-        }
-        return value;
-    }
-
-    subArray(args) {
-        let value = args[0];
-        for (let i = 0; i < args.length; i++) {
-            value -= args[i];
+        for (const item of args) {
+            value += item;
         }
         return value;
     }
 
     multiplyArray(args) {
         let value = args[0];
-        for (let i = 0; i < args.length; i++) {
+        const length = args.length;
+        for (let i = 1; i < length; i++) {
             value *= args[i];
         }
         return value;
